d04790ad469da14e2695d9428d2ffa88225352f7
LuckyPigoen <lucky90322@gmail.com>
2018-06-19 22:45:42 +0800

    Add read from txt file method for inputs script

--- inputs.py
+++ inputs.py
@@ -11,5 +11,7 @@ conn = psycopg2.connect(
 
 cursor = connection.cursor()
 '''
+# f = open('LSA.txt', 'r')
+# text = f.read()
 # r = requests.post(ip, data={'keywords':sys.argv[1]})
 os.system('python3 function.py ' + sys.argv[1])
