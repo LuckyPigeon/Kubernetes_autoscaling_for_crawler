16d8d9fc5d81e984f230eb707d638f00f1688ea9
LuckyPigoen <lucky90322@gmail.com>
2018-06-20 19:08:09 +0800

    Finish function script

--- Function/function.py
+++ Function/function.py
@@ -1,19 +1,27 @@
-import re, sys, threading
+import re, sys, threading, mysql.connector
 from urllib.request import urlopen
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from flask import Flask, request
 
 '''Constant url'''
 url = 'https://www.youtube.com/results?search_query='
 
-def crawler(inputs):
+app = Flask(__name__)
+
+@app.route('crawler', method=['GET'])
+def crawler():
+	inputs = request.args.get('text')
+
 	'''Driver'''
 	driver = webdriver.Chrome('./chromedriver')
-	driver.get(inputs)
+	driver.get(url + inputs)
+	# driver.get(inputs)
 
 	'''Analyze page source'''
 	source = driver.page_source
 	data = source.split('<script>')
+	driver.quit()
 
 	for i in range(len(data)):
 		if data[i].find('twoColumnSearch') != -1:
@@ -35,8 +43,14 @@ def crawler(inputs):
 	rowdata = [re.sub('}}', '', x) for x in rowdata]
 	rowdata = [re.sub(r'\\u0026', '', x) for x in rowdata]
 
-f = open('LSA.txt', 'r')
-req = [threading.Thread(target = crawler, args = (text)) for text in f.realines()]
+	conn = mysql.connector.connect(host = 'localhost', database = 'Crawler', user = 'luckypigeon', password = 'lucky90322')
+	cursor = conn.cursor()
 
-for i in range(len(req)):
-	req[i].start()
+	query = "INSERT INTO result(keyword, url, title) VALUES(%s, %s, %s)"
+	
+	for i in range(len(urldata)):
+		cursor.execute(query, inputs, urldata[i], rowdata[i])
+
+	return rowdata, urldata
+
+app.run(debug = True
